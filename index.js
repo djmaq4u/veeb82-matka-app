bf8da1f2c40fc0b6b92b11d8b6a3b4029fe18274
Magnus Volar <magnusvolar@hotmail.com>
2021-05-26 13:32:19 +0300

    proovimuudatused

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
+// näitefunktsioon loengust 11
+function aeg(req, res) {
+  const aeg = new Date()
+  res.send("Kellaaeg on: " + aeg.toString())
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/aeg', aeg) // ise lisatud
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  // 1 staatilise kataloogi asukoht
+  // mall on html doc, milles on html-ile mitteomaseid kohti, %-tagid on mallimuutujad
+  // ühine menüü jm
+  // 2 ja 3 mallisüsteem, mallid on kataloogis "views"
+  // mallisüsteem ejs
+  // get funktsioon, 
