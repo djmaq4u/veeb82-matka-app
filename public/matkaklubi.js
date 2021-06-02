8f87363ae223a18d4207326b1b09aa1a155ace5f
Magnus Volar <magnusvolar@hotmail.com>
2021-06-02 11:20:35 +0300

    matkaklubi esileht

---
+++ public/matkaklubi.js
@@ -0,0 +1,61 @@
+console.log("test")
+
+const matk1 = {
+    nimetus: "Jalgsimatk Kõrvemaal",
+    pildiURL: "./pildid/BMW_pealtnagija_foto.jpg",
+    kohti: 12,
+    kirjeldus: "Kõnnime palju aga loodus on ilus. Lõuna lõkkel.",
+    registreerunud: ['Mati', 'Kati', 'Rebase-Rein', 'Jänku Juta'],
+}
+
+const matk2 = {
+    nimetus: "Süstamatk ümber Hiiumaa",
+    pildiURL: "./pildid/klassikas.jpg",
+    kohti: 6,
+    kirjeldus: "Sõidame palju aga loodus on ilus. Lõuna lõkkel.",
+    registreerunud: ['Rebase-Rein', 'Jänku Juta'],
+}
+
+const matk3 = {
+    nimetus: "Jalgrattamatk Virumaal",
+    pildiURL: "./pildid/126_2667.JPG",
+    kohti: 10,
+    kirjeldus: "Sõidame palju aga loodus on ilus. Lõuna lõkkel.",
+    registreerunud: ['Rebase-Rein', 'Jänku Juta'],
+}
+
+const matk4 = {
+    nimetus: "Kepikõnnimatk ümber Tartu",
+    pildiURL: "./pildid/Mon_25.bmp",
+    kohti: 2,
+    kirjeldus: "Sõidame palju aga loodus on ilus. Lõuna lõkkel.",
+    registreerunud: ['Järviste Andres'],
+}
+
+const matkad = [matk1, matk2, matk3, matk4]
+
+function naitaMatkaAndmeid(matk) {
+    const valjundElement = document.getElementById("matkade_valjund")
+    const vabadKohad = matk.kohti - matk.registreerunud.length
+    let valjundHtml = `
+        <div class="col-md-4 card">
+            <img class="card-img-top" src="${matk.pildiURL}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">${matk.nimetus}</h4>
+                <p class="card-text">
+                   ${matk.kirjeldus}
+                </p>
+                <p class="card-text">
+                   ${matk.kirjeldus}
+                </p>
+                <a href="#" class="btn btn-success">Registreeru</a>
+            </div>
+        </div>
+    `
+
+    valjundElement.innerHTML += valjundHtml
+}
+
+for (const matk of matkad) {
+    naitaMatkaAndmeid(matk)
+}
