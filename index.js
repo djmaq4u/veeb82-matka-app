9c39eb11eb0521fed7e2582946cdbfb9bdf6def5
Magnus Volar <magnusvolar@hotmail.com>
2021-06-04 11:58:10 +0300

    muudatused

--- index.js
+++ index.js
@@ -47,15 +47,17 @@ function aeg(req, res) {
 // KODUTÖÖ -- kui sisest. vale nr, siis anda teade: a'la "sellist matka pole olemas"
 function registreeru (req, res) {
 
-  if (isNaN(req.params.matk)) {
+  //if (isNaN(req.params.matk)) {
+  
+  const matk = parseInt(req.params.matk)
+    if (isNaN(matk)) {
     return res.render("pages/viga", {veateade: "Ilmnes viga: ssisesta siiski matka-number :)"})
   }
 
-  if (req.params.matk >= matkad.length) {
+  //if (req.params.matk >= matkad.length) {
+    if (matk >= matkad.length) {
     return res.render("pages/viga", {veateade: "Ilmnes viga: sellist matkanumbrit meil pole :(("})
   }
-  
-  const matk = req.params.matk
   const matkaAndmed = matkad[matk]
   //renderdame malli
   res.render("pages/registreeru", {matkaNumber: matk, matkaAndmed: matkaAndmed})
